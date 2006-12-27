7e7e85c2e5e194dd3b748845eb04cecb775025c6
Stuart Gathman <stuart@gathman.org>
2006-12-27 00:55:22 +0000

    pygossip server.

---
+++ gossip/SocketServer.py
@@ -0,0 +1,50 @@
+# Copyright 2005 Business Management Systems, Inc
+# Author: Stuart D. Gathman
+# Distributed under the terms of the GNU General Public License version 2
+# See COPYING for details
+
+import SocketServer
+import logging
+
+log = logging.getLogger('gossip')
+
+class Handler(SocketServer.BaseRequestHandler):
+
+  def readline(self):
+    ssl = self.request
+    buf = self.buf
+    pos = buf.find('\012')
+    while pos < 0:
+      buf += ssl.recv(256)
+      pos = buf.find('\012')
+    self.buf = buf[pos+1:]
+    return buf[:pos]
+
+  def handle(self):
+    log.debug("connect")
+    ssl = self.request
+    gossip = self.server.gossip
+    self.buf = ''
+    while True:
+      try:
+        buf = self.readline()
+	if buf == '': break
+        resp = gossip.do_request(buf)
+	if resp:
+	  ssl.send(resp+'\012\012')
+      except EOFError:
+        log.debug("Ending connection")
+	return
+
+class Daemon(object):
+
+  def __init__(self,gossip,addr='0.0.0.0',port=11900):
+    self.gossip = gossip
+    server_addr = (addr,port)
+    self.server = SocketServer.ThreadingTCPServer(server_addr,Handler)
+    self.server.daemon = self
+    self.server.gossip = gossip
+
+  def run(self):
+    self.server.serve_forever()
+    #self.server.handle_request()
