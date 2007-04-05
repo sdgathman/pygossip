f906fd175c0fc8a359746364dc35778df3a06e02
Stuart Gathman <stuart@gathman.org>
2007-04-05 18:50:22 +0000

    Make client threadsafe.

--- gossip/client.py
+++ gossip/client.py
@@ -4,6 +4,7 @@
 # See COPYING for details
 
 import socket
+import thread
 from gossip import log
 
 # I've double checked, and the double newline (actually '\012\012') is in 
@@ -20,22 +21,12 @@ class Gossip(object):
     self.sock = None
     self.buf = ''
     self.persistent = True
+    self.lock = thread.allocate_lock()
 
   def get_iplist(self):
     host,port = self.node
     host,aliases,iplist = socket.gethostbyname_ex(host)
     return iplist
-
-  # from smtplib.  I had hoped there was a better way than reading
-  # one char at a time.
-  def readline1(self):
-      str = ""
-      chr = None
-      ssl = self.sock
-      while chr != "\n":
-	  chr = ssl.recv(1)
-	  str += chr 
-      return str
 
   # hopefully a better readline
   def readline(self):
@@ -48,45 +39,44 @@ class Gossip(object):
     self.buf = buf[pos+1:]
     return buf[:pos]
 
-  # sock.sendall() gets Broken Pipe exception (why?)
-  def sendall(self,s):
-    ssl = self.request
-    n = ssl.send(s)
-    while n < len(s):
-      if n > 0: s = s[n:]
-      n = ssl.send(s)
+  def sendreq(self,req,void=False):
+    self.lock.acquire()
+    try:
+      try:
+        sock = self.sock
+        if not sock:
+          sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+          sock.connect(self.node)
+          self.sock = sock
+          self.buf = ''
+        if self.test:
+          b = len(req)/2
+          sock.sendall(req[:b])
+          sock.sendall(req[b:])
+        else:
+          sock.sendall(req)
+        buf = None
+        if not void:
+          while not buf:	# skip empty lines
+            buf = self.readline()
+        if not self.persistent:
+          sock.close()
+          self.sock = None
+        return buf
+      except socket.error,x:
+        log.error('%s: %s',self.node[0],x)
+        if sock: sock.close()
+        self.sock = None
+        return None
+    finally:
+      self.lock.release()
 
   def query(self,umis,id,qual,ttl=1):
-    try:
-      sock = self.sock
-      if not sock:
-	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-	sock.connect(self.node)
-	self.sock = sock
-	self.buf = ''
-      req = "Q:%s:%s:%d:%s%s" % (id,qual,ttl,umis,EOL)
-      if self.test:
-	b = len(req)/2
-	sock.sendall(req[:b])
-	sock.sendall(req[b:])
-      else:
-	sock.sendall(req)
-      buf = None
-      while not buf:	# skip empty lines
-        buf = self.readline()
-      if not self.persistent:
-        sock.close()
-	self.sock = None
-      umis,hdr,val = buf.strip().split(None,2)
-      return umis,hdr.rstrip(':'),val
-    except socket.error,x:
-      log.error('%s: %s',self.node[0],x)
-      if sock: sock.close()
-      self.sock = None
-      return None
+    req = "Q:%s:%s:%d:%s%s" % (id,qual,ttl,umis,EOL)
+    buf = self.sendreq(req)
+    umis,hdr,val = buf.strip().split(None,2)
+    return umis,hdr.rstrip(':'),val
 
   def feedback(self,umis,spam):
-    sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    sock.connect(self.node)
     req = "F:%s:%s%s" % (umis,spam,EOL)
-    sock.send(req)
+    self.sendreq(req,void=True)
